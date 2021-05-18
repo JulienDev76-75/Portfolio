d3c12cbf8ca78f3997dce2587e63c5416ac2ba8a
JulienDev76-75 <julien.coart@gmail.com>
2021-05-18 14:17:27 +0200

    new gh page deployed

--- src/components/projets/cardsprojects/Shifumi.js
+++ src/components/projets/cardsprojects/Shifumi.js
@@ -4,7 +4,7 @@ import Data from './shifumi.json';
 
 function Shifoumi () {
     return (
-        <div className="scale-in-ver-center col-12 col-sm-12 col-md-12 col-lg-4 py-3 bg-transparent">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-4 py-3 bg-transparent">
             {Data.map((post) => {
                 return (
                     <div>
@@ -18,9 +18,29 @@ function Shifoumi () {
                                     <p className="text-white">{post.contenu}</p>
                                     <button type="button" className="btn btn-dark"><a className="voirleprojet" href={post.link} target="_blank" rel="noreferrer noopener">Voir le projet</a>
                                     </button>
+                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                        Launch demo modal
+                                    </button>
                                 </div>
                             </div>
-                        </div>  
+                        </div> 
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        ...
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" className="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
                     </div>
                 );
             })}
